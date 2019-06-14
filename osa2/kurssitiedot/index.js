05c6505959e6abb8661bcce02351be986f7e7642
Pomppupallo <tonikvirtanen@gmail.com>
2019-06-14 14:33:54 +0300

    Create index.js

---
+++ osa2/kurssitiedot/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Course from './components/Course'
+
+const App = () => {
+
+    const courses = [ 
+    {
+        name: 'Half Stack -sovelluskehitys',
+        id: 1,
+        parts: [
+            {
+            name: 'Reactin perusteet',
+            exercises: 10,
+            id: 1
+            },
+            {
+            name: 'Matematiikka',
+            exercises: 15,
+            id: 2
+            },
+            {
+            name: 'Tiedonvälitys propseilla',
+            exercises: 7,
+            id: 3
+            },
+            {
+            name: 'Komponenttien tila',
+            exercises: 14,
+            id: 4
+            }
+        ]
+    },
+    {
+        name: 'Node.js',
+        id: 2,
+        parts: [
+          {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+          },
+          {
+            name: 'Middlewaret',
+            exercises: 7,
+            id: 2
+          }
+        ]
+      }
+    ]
+
+    return (
+      <div>
+        <h1>Opetusohjelma</h1>
+        <Course course = {courses} />
+      </div>
+    )
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('root'))
